90f7fc4e7aaf9e478f353ca8b4743297a5bbc90b
cmh104u <andy4042023@gmail.com>
2020-12-06 00:01:21 +0800

    [gen.py] remove useless code segment

--- gen.py
+++ gen.py
@@ -1,28 +1,14 @@
 # generate .txt file for training input
-# python gen.py [target directory] [output directory] [frame gap]
+# python gen.py [target directory] [output directory] [frame gap] [frame count]
 import os
 from os.path import isdir, join
 import glob
 import sys
 
-def rename_data(path, key):
-	for data_path in glob.glob(join(path, '*.tiff')):
-		cut_pos = len(join(path, key))
-		idx = data_path[cut_pos:].split('.')[0]
-
-		# pad index to length 5
-		padding_zero_cnt = 5 - len(idx)
-		if padding_zero_cnt < 0:
-			print('Error in rename_data()')
-		else:
-			padding_zero = '0' * padding_zero_cnt
-			idx = '_' + padding_zero + idx
-
-		os.rename(data_path, join(path, key + idx + '.tiff'))
-
 target_dir = sys.argv[1]
 output_dir = sys.argv[2]
 gap = int(sys.argv[3])
+frame_cnt = int(sys.argv[4])
 
 if not isdir(output_dir):
 	os.mkdir(output_dir)
@@ -40,17 +26,13 @@ print('Begin!')
 # for else class in training data
 for file in files:
 	if isdir(join(training_data_path, file)):
-		# rename data e.g. 5HT1A-F-90001110 to 5HT1A-F-900011_00010
-		#rename_data(join(training_data_path, file, 'projection'), file)
-
 		#write .txt file
-		file_cnt = 179 // gap + 1
-		#file_cnt = len(os.listdir(join(training_data_path, file, 'projection')))
+		file_cnt = (frame_cnt - 1) // gap + 1
 		cnt = 0
 		idx = 0
 		for path in sorted(glob.glob(join(training_data_path, file, '*.png'))):
 			idx += 1
-			if idx % gap != 1:
+			if gap != 1 and idx % gap != 1:
 				continue
 			cnt += 1
 
